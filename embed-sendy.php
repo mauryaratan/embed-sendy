b741a6eab6b1deb72211f76e5d71e4f929b12c88
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-04-20 22:27:27 +0530

    Refresh `Embed_Sendy` class

--- embed-sendy.php
+++ embed-sendy.php
@@ -18,15 +18,127 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Main Embed_Sendy class.
+ *
+ * @since 1.0.0.
+ */
 final class Embed_Sendy {
-	public function __construct() {
+	/**
+	 * Class instance.
+	 *
+	 * @var Embed_Sendy The one true Embed_Sendy
+	 * @since 1.0.0
+	 */
+	private static $instance;
 
+	/**
+	 * Main Embed_Sendy Instance.
+	 *
+	 * Insures that only one instance of Easy_Digital_Downloads exists in memory at any one
+	 * time. Also prevents needing to define globals all over the place.
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Embed_Sendy ) ) {
+			self::$instance = new Embed_Sendy();
+			self::$instance->setup_constants();
+			self::$instance->includes();
+
+			add_shortcode( 'embed_sendy', array( self::$instance, 'embed_sendy_shortcode' ) );
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Setup plugin constants.
+	 *
+	 * @access private
+	 * @since 1.0.0
+	 * @return void
+	 */
+	private function setup_constants() {
+		// Plugin version.
+		if ( ! defined( 'ESD_VERSION' ) ) {
+			define( 'ESD_VERSION', '1.0.0' );
+		}
+
+		// Plugin Folder Path.
+		if ( ! defined( 'ESD_PLUGIN_DIR' ) ) {
+			define( 'ESD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		}
+
+		// Plugin Folder URL.
+		if ( ! defined( 'ESD_PLUGIN_URL' ) ) {
+			define( 'ESD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		}
+
+		// Plugin Root File.
+		if ( ! defined( 'ESD_PLUGIN_FILE' ) ) {
+			define( 'ESD_PLUGIN_FILE', __FILE__ );
+		}
+	}
+
+	/**
+	 * Include plugins files.
+	 *
+	 * @access private
+	 * @since 1.0.0
+	 * @return void
+	 */
+	private function includes() {
+		require_once ESD_PLUGIN_DIR . 'src/init.php';
+		require_once ESD_PLUGIN_DIR . 'includes/class-wp-osa.php';
+		require_once ESD_PLUGIN_DIR . 'includes/admin-settings.php';
+	}
+
+	/**
+	 * Add [embed_sendy] shortcode.
+	 *
+	 * @param string|mixed $atts Shortcode attributes.
+	 * @return mixed
+	 */
+	public function embed_sendy_shortcode( $atts ) {
+		$atts = shortcode_atts( array(
+			'list' => '',
+		), $atts, 'embed_sendy' );
+
+		ob_start();
+
+
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Format all Sendy lists as an associative array.
+	 *
+	 * @return array|void Returns array of List Name -> List ID.
+	 */
+	public function get_lists() {
+		$settings = get_option( 'esd_settings' );
+
+		if ( array_key_exists( 'esd_lists', $settings ) ) {
+			$new_lists = [];
+
+			foreach ( $settings['esd_lists'] as $lists ) {
+				$new_lists[ $lists[1] ] = $lists[0];
+			}
+
+			return $new_lists;
+		}
+
+		return false;
 	}
 }
 
 /**
- * Block Initializer.
+ * The main function for that returns Embed_Sendy.
+ *
+ * @return object|Embed_Sendy
  */
-require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-osa.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/admin-settings.php';
+function ESD() { //@codingStandardsIgnoreLine
+	return Embed_Sendy::instance();
+}
+
+ESD();
